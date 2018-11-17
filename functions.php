e4b4939e9d2deb486c04ed2e4aedd3282b6c9dd0
timatron <timatron@users.noreply.github.com>
2018-11-17 13:24:09 -0800

    maybe now

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function rs_theme_enqueue_styles() {
 	$parent_style = 'lodestar';
-	wp_dequeue_script( 'lodestar-css' );
+	wp_dequeue_script( 'lodestar-style' );
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style',
 		get_stylesheet_directory_uri() . '/style.css',
