82d41c26bcaa71f94035579a1d5cfa45b0a347cb
timatron <timatron@users.noreply.github.com>
2018-11-17 13:26:53 -0800

    ❤️

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
 function rs_theme_enqueue_styles() {
 	$parent_style = 'lodestar';
 	wp_dequeue_style( 'lodestar-style' );
+	wp_dequeue_style( 'lodestar-css' );
 	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style',
 		get_stylesheet_directory_uri() . '/style.css',
