49727ec2b744b754c33ebe23bb0d43f068094d5f
timatron <timatron@users.noreply.github.com>
2018-11-17 13:33:06 -0800

    dont enqueue in child for parent

--- functions.php
+++ functions.php
@@ -2,11 +2,6 @@
 
 function rs_theme_enqueue_styles() {
 	$parent_style = 'lodestar';
-
-	wp_dequeue_style( 'lodestar-style' );
-	wp_dequeue_style( 'lodestar' );
-
-	wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
 	wp_enqueue_style( 'child-style',
 		get_stylesheet_directory_uri() . '/style.css',
 		array( $parent_style ),
